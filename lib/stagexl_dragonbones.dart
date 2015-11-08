19ca9ade9a0a002a1c04eb90614b90a774c0a8d0
Bernhard <bernhard.robert.pichler@gmail.com>
2015-11-08 10:56:53 +0100

    Added math tools

--- lib/stagexl_dragonbones.dart
+++ lib/stagexl_dragonbones.dart
@@ -16,11 +16,12 @@ part 'src/data/skin.dart';
 part 'src/data/skin_slot.dart';
 part 'src/data/slot.dart';
 
+part 'src/math/tools.dart';
 part 'src/math/transform.dart';
-part 'src/parser/dragon_bones_parser_json4.dart';
 
 part 'src/skeleton/skeleton.dart';
 part 'src/skeleton/skeleton_bone.dart';
 part 'src/skeleton/skeleton_slot.dart';
 
 part 'src/dragon_bones.dart';
+part 'src/dragon_bones_parser_json4.dart';
