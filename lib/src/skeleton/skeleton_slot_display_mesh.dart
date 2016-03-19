c8b7e56e8257f0a6b289d937924e12137938a7a7
Bernhard <bernhard.robert.pichler@gmail.com>
2016-03-19 16:30:42 +0100

    Render the mesh objects
    The free form deformation is still missing, coming soon ....

--- lib/src/skeleton/skeleton_slot_display_mesh.dart
+++ lib/src/skeleton/skeleton_slot_display_mesh.dart
@@ -7,16 +7,49 @@ class SkeletonSlotDisplayMesh extends SkeletonSlotDisplay {
   SkeletonSlotDisplayMesh(DisplayMesh display, this.renderTextureQuad)
       : super(display) {
 
-    var pivotX = renderTextureQuad.targetWidth / 2;
-    var pivotY = renderTextureQuad.targetHeight / 2;
-    this.matrix.prependTranslation(-pivotX, -pivotY);
+    // var pivotX = renderTextureQuad.targetWidth / 2;
+    // var pivotY = renderTextureQuad.targetHeight / 2;
+    // this.matrix.prependTranslation(-pivotX, -pivotY);
   }
 
   //---------------------------------------------------------------------------
 
   @override
   void render(RenderState renderState) {
-   // renderState.renderTextureQuad(this.renderTextureQuad);
+
+    // TODO: add Free Form Deformation
+    // TODO: make it fast, this is just a proof of concept
+
+    var renderTextureQuad = this.renderTextureQuad;
+    var renderTexture = renderTextureQuad.renderTexture;
+    var samplerMatrix = renderTextureQuad.samplerMatrix;
+
+    var ma = samplerMatrix.a * renderTextureQuad.targetWidth;
+    var mb = samplerMatrix.b * renderTextureQuad.targetWidth;
+    var mc = samplerMatrix.c * renderTextureQuad.targetHeight;
+    var md = samplerMatrix.d * renderTextureQuad.targetHeight;
+    var mx = samplerMatrix.tx;
+    var my = samplerMatrix.ty;
+
+    var displayMesh = this.display as DisplayMesh;
+    var vertices = displayMesh.vertices;
+    var uvs = displayMesh.uvs;
+    var ixList = displayMesh.triangles;
+    var vxList = new Float32List(vertices.length + uvs.length);
+
+    for (int i = 0, o = 0; i < vertices.length - 1; i += 2, o += 4) {
+      vxList[i * 2 + 0] = vertices[i + 0];
+      vxList[i * 2 + 1] = vertices[i + 1];
+    }
+
+    for (int i = 0, o = 0; i < uvs.length - 1; i+= 2, o += 4) {
+      var u = uvs[i + 0];
+      var v = uvs[i + 1];
+      vxList[o + 2] = u * ma + v * mc + mx;
+      vxList[o + 3] = u * mb + v * md + my;
+    }
+
+    renderState.renderTextureMesh(renderTexture, ixList, vxList);
   }
 }
 
