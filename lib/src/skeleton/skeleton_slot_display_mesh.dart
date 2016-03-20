5d9680c1cedf8e768154dc32143cef698ea36fc8
Bernhard <bernhard.robert.pichler@gmail.com>
2016-03-20 08:49:50 +0100

    Optimized SkeletonSlotDisplayMesh

--- lib/src/skeleton/skeleton_slot_display_mesh.dart
+++ lib/src/skeleton/skeleton_slot_display_mesh.dart
@@ -3,13 +3,33 @@ part of stagexl_dragonbones;
 class SkeletonSlotDisplayMesh extends SkeletonSlotDisplay {
 
   final RenderTextureQuad renderTextureQuad;
+  final Float32List vxList;
+  final Int16List ixList;
 
-  SkeletonSlotDisplayMesh(DisplayMesh display, this.renderTextureQuad)
-      : super(display) {
+  SkeletonSlotDisplayMesh(DisplayMesh mesh, this.renderTextureQuad)
+      : this.vxList = new Float32List(mesh.vertices.length + mesh.uvs.length),
+        this.ixList = mesh.triangles,
+        super(mesh) {
 
-    // var pivotX = renderTextureQuad.targetWidth / 2;
-    // var pivotY = renderTextureQuad.targetHeight / 2;
-    // this.matrix.prependTranslation(-pivotX, -pivotY);
+    var samplerMatrix = this.renderTextureQuad.samplerMatrix;
+    var ma = samplerMatrix.a * renderTextureQuad.targetWidth;
+    var mb = samplerMatrix.b * renderTextureQuad.targetWidth;
+    var mc = samplerMatrix.c * renderTextureQuad.targetHeight;
+    var md = samplerMatrix.d * renderTextureQuad.targetHeight;
+    var mx = samplerMatrix.tx;
+    var my = samplerMatrix.ty;
+
+    for (int i = 0, o = 0; i < mesh.vertices.length - 1; i += 2, o += 4) {
+      vxList[i * 2 + 0] = mesh.vertices[i + 0];
+      vxList[i * 2 + 1] = mesh.vertices[i + 1];
+    }
+
+    for (int i = 0, o = 0; i < mesh.uvs.length - 1; i += 2, o += 4) {
+      var u = mesh.uvs[i + 0];
+      var v = mesh.uvs[i + 1];
+      vxList[o + 2] = u * ma + v * mc + mx;
+      vxList[o + 3] = u * mb + v * md + my;
+    }
   }
 
   //---------------------------------------------------------------------------
@@ -18,36 +38,8 @@ class SkeletonSlotDisplayMesh extends SkeletonSlotDisplay {
   void render(RenderState renderState) {
 
     // TODO: add Free Form Deformation
-    // TODO: make it fast, this is just a proof of concept
 
-    var renderTextureQuad = this.renderTextureQuad;
-    var renderTexture = renderTextureQuad.renderTexture;
-    var samplerMatrix = renderTextureQuad.samplerMatrix;
-
-    var ma = samplerMatrix.a * renderTextureQuad.targetWidth;
-    var mb = samplerMatrix.b * renderTextureQuad.targetWidth;
-    var mc = samplerMatrix.c * renderTextureQuad.targetHeight;
-    var md = samplerMatrix.d * renderTextureQuad.targetHeight;
-    var mx = samplerMatrix.tx;
-    var my = samplerMatrix.ty;
-
-    var displayMesh = this.display as DisplayMesh;
-    var vertices = displayMesh.vertices;
-    var uvs = displayMesh.uvs;
-    var ixList = displayMesh.triangles;
-    var vxList = new Float32List(vertices.length + uvs.length);
-
-    for (int i = 0, o = 0; i < vertices.length - 1; i += 2, o += 4) {
-      vxList[i * 2 + 0] = vertices[i + 0];
-      vxList[i * 2 + 1] = vertices[i + 1];
-    }
-
-    for (int i = 0, o = 0; i < uvs.length - 1; i+= 2, o += 4) {
-      var u = uvs[i + 0];
-      var v = uvs[i + 1];
-      vxList[o + 2] = u * ma + v * mc + mx;
-      vxList[o + 3] = u * mb + v * md + my;
-    }
+    var renderTexture = this.renderTextureQuad.renderTexture;
 
     renderState.renderTextureMesh(renderTexture, ixList, vxList);
   }
