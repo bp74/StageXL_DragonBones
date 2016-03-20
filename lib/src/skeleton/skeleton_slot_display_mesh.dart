1249b44fe4c3b7c9093ef4d29146578cfc3d3cde
Bernhard <bernhard.robert.pichler@gmail.com>
2016-03-20 09:06:28 +0100

    Simplified SkeletonSlotDisplayMesh

--- lib/src/skeleton/skeleton_slot_display_mesh.dart
+++ lib/src/skeleton/skeleton_slot_display_mesh.dart
@@ -11,24 +11,20 @@ class SkeletonSlotDisplayMesh extends SkeletonSlotDisplay {
         this.ixList = mesh.triangles,
         super(mesh) {
 
-    var samplerMatrix = this.renderTextureQuad.samplerMatrix;
-    var ma = samplerMatrix.a * renderTextureQuad.targetWidth;
-    var mb = samplerMatrix.b * renderTextureQuad.targetWidth;
-    var mc = samplerMatrix.c * renderTextureQuad.targetHeight;
-    var md = samplerMatrix.d * renderTextureQuad.targetHeight;
-    var mx = samplerMatrix.tx;
-    var my = samplerMatrix.ty;
+    var sm = renderTextureQuad.samplerMatrix;
+    var tw = renderTextureQuad.targetWidth;
+    var th = renderTextureQuad.targetHeight;
 
     for (int i = 0, o = 0; i < mesh.vertices.length - 1; i += 2, o += 4) {
-      vxList[i * 2 + 0] = mesh.vertices[i + 0];
-      vxList[i * 2 + 1] = mesh.vertices[i + 1];
+      vxList[o + 0] = mesh.vertices[i + 0];
+      vxList[o + 1] = mesh.vertices[i + 1];
     }
 
     for (int i = 0, o = 0; i < mesh.uvs.length - 1; i += 2, o += 4) {
-      var u = mesh.uvs[i + 0];
-      var v = mesh.uvs[i + 1];
-      vxList[o + 2] = u * ma + v * mc + mx;
-      vxList[o + 3] = u * mb + v * md + my;
+      var x = mesh.uvs[i + 0] * tw;
+      var y = mesh.uvs[i + 1] * th;
+      vxList[o + 2] = x * sm.a + y * sm.c + sm.tx;
+      vxList[o + 3] = x * sm.b + y * sm.d + sm.ty;
     }
   }
 
